8a1e6a358159bbc9f3e42926ab5701391859f42e
Venkatesh664 <103093498+Venkatesh664@users.noreply.github.com>
2025-05-28 10:40:57 +0530

    Update SbrDemoApplication.java

--- src/main/java/com/dailycodework/sbrdemo/SbrDemoApplication.java
+++ src/main/java/com/dailycodework/sbrdemo/SbrDemoApplication.java
@@ -1,13 +1,17 @@
-package com.dailycodework.sbrdemo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SbrDemoApplication {
+public class SbrServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(SbrDemoApplication.class, args);
+        SpringApplication.run(SbrServerApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SbrServerApplication.class);
+    }
 }
